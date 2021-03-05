cf4a3bf50839fa1103369b28c95dcb44f6f2e8a0
Sebbbbe <sebastian.tiger901@gmail.com>
2021-03-05 18:22:49 +0100

    plus works now

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace calculater_training
         string empty = "";
         float number1 = 0;
         float total = 0;
+        float totalNumbers = 0;
         public MainWindow()
         {
             InitializeComponent();
@@ -30,15 +31,91 @@ namespace calculater_training
       
 	
 
+
         private void btn1_Click(object sender, RoutedEventArgs e)
         {
-
-
-            total = (number1 * 10) + 1;
-                txtBox.Text = number1.ToString();
-       
+            number1 = 1;
+            total = (total * 10) + number1;
+            txtBox.Text = total.ToString();
         }
 
-        
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            number1 = 2;
+            total = (total * 10) + number1;
+            txtBox.Text = total.ToString();
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            number1 = 3;
+            total = (total * 10) + number1;
+            txtBox.Text = total.ToString();
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            number1 = 4;
+            total = (total * 10) + number1;
+            txtBox.Text = total.ToString();
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            number1 = 5;
+            total = (total * 10) + number1;
+            txtBox.Text = total.ToString();
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            number1 = 6;
+            total = (total * 10) + number1;
+            txtBox.Text = total.ToString();
+        }
+
+        private void btn7_Click(object sender, RoutedEventArgs e)
+        {
+            number1 = 7;
+            total = (total * 10) + number1;
+            txtBox.Text = total.ToString();
+        }
+
+        private void btn8_Click(object sender, RoutedEventArgs e)
+        {
+            number1 = 8;
+            total = (total * 10) + number1;
+            txtBox.Text = total.ToString();
+        }
+
+        private void btn9_Click(object sender, RoutedEventArgs e)
+        {
+            number1 = 9;
+            total = (total * 10) + number1;
+            txtBox.Text = total.ToString();
+        }
+
+        private void btn0_Click(object sender, RoutedEventArgs e)
+        {
+            number1 = 0;
+            total = (total * 10) + number1;
+            txtBox.Text = total.ToString();
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            txtBox.Text = "0";
+            totalNumbers = total;
+            total = 0;
+        }
+
+        private void btnEquals_Click(object sender, RoutedEventArgs e)
+        {
+            totalNumbers = totalNumbers + total;
+            txtBox.Text = totalNumbers.ToString();
+            total = 0;
+            totalNumbers = 0;
+
+        }
     }
 }
