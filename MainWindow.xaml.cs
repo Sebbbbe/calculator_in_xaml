8bfa6e7c1f4027b2e8af22c7179549ced2246dd9
Sebbbbe <sebastian.tiger901@gmail.com>
2021-03-07 14:03:16 +0100

    default layout do changes now!

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace calculater_training
 {
@@ -7,292 +9,86 @@ namespace calculater_training
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string operation = "";
 
-        private float number1 = 0;
-        private float total = 0;
-        private float totalNumbers;
-        private float decimalNumber = 0.1F;
-        private float x = 0.1F;
 
-        private bool isDecimal = false;
-        private bool isNegative = false;
+        double value = 0;
+        
+        string operation = "";
+        bool operationPressed = false;
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void btn1_Click(object sender, RoutedEventArgs e)
+        private void btn_Click(object sender, RoutedEventArgs e)
         {
-            number1 = 1;
-            IsNegative();
-            if (isDecimal == true)
+            if (result.Text =="0" || operationPressed == true)
             {
-                IsDecimal();
+                result.Clear();
+               
             }
-            else
-            {
-                ShowNumber();
-            }
+
+            Button b = (Button)sender;
+            result.Text = result.Text + b.Content;
+            operationPressed = false;
+
         }
 
-        private void btn2_Click(object sender, RoutedEventArgs e)
+        private void operator_click(object sender, RoutedEventArgs e)
+
         {
-            number1 = 2;
-            IsNegative();
-            if (isDecimal == true)
-            {
-                IsDecimal();
-            }
-            else
-            {
-                ShowNumber();
-            }
-        }
+            Button b = (Button)sender;
+            operation = (string)(b.Content);
 
-        private void btn3_Click(object sender, RoutedEventArgs e)
-        {
-            number1 = 3;
-            IsNegative();
-            if (isDecimal == true)
-            {
-                IsDecimal();
-            }
-            else
-            {
-                ShowNumber();
-            }
-        }
 
-        private void btn4_Click(object sender, RoutedEventArgs e)
-        {
-            number1 = 4;
-            IsNegative();
-            if (isDecimal == true)
-            {
-                IsDecimal();
-            }
-            else
-            {
-                ShowNumber();
-            }
-        }
+            value = double.Parse(result.Text);
+            operationPressed = true;
+            equation.Content = value + " " + operation;
 
-        private void btn5_Click(object sender, RoutedEventArgs e)
-        {
-            number1 = 5;
-            IsNegative();
-            if (isDecimal == true)
-            {
-                IsDecimal();
-            }
-            else
-            {
-                ShowNumber();
-            }
-        }
+          
 
-        private void btn6_Click(object sender, RoutedEventArgs e)
-        {
-            number1 = 6;
-            IsNegative();
-            if (isDecimal == true)
-            {
-                IsDecimal();
-            }
-            else
-            {
-                ShowNumber();
-            }
-        }
 
-        private void btn7_Click(object sender, RoutedEventArgs e)
-        {
-            number1 = 7;
-            IsNegative();
-            if (isDecimal == true)
-            {
-                IsDecimal();
-            }
-            else
-            {
-                ;
-                ShowNumber();
-            }
-        }
 
-        private void btn8_Click(object sender, RoutedEventArgs e)
-        {
-            number1 = 8;
-            IsNegative();
-            if (isDecimal == true)
-            {
-                IsDecimal();
-            }
-            else
-            {
-                ShowNumber();
-            }
-        }
-
-        private void btn9_Click(object sender, RoutedEventArgs e)
-        {
-            number1 = 9;
-            IsNegative();
-            if (isDecimal == true)
-            {
-                IsDecimal();
-            }
-            else
-            {
-                ShowNumber();
-            }
-        }
-
-        private void btn0_Click(object sender, RoutedEventArgs e)
-        {
-            number1 = 0;
-            if (isDecimal == true)
-            {
-                IsDecimal();
-            }
-            else
-            {
-                ShowNumber();
-            }
-        }
-
-        private void btnPlus_Click(object sender, RoutedEventArgs e)
-        {
-            isDecimal = false;
-            isNegative = false;
-
-            operation = "+";
-            ResetTextBox();
-        }
-
-        private void btnMultiply_Click(object sender, RoutedEventArgs e)
-        {
-            isDecimal = false;
-            isNegative = false;
-
-            operation = "*";
-            ResetTextBox();
-        }
-
-        private void btnMinus_Click(object sender, RoutedEventArgs e)
-        {
-            isDecimal = false;
-            isNegative = false;
-
-            if (txtBox.Text == "0")
-            {
-                txtBox.Text = "-0";
-                isNegative = true;
-            }
-            else
-            {
-                ResetTextBox();
-                operation = "-";
-            }
-        }
-
-        private void btnDivided_Click(object sender, RoutedEventArgs e)
-        {
-            isDecimal = false;
-            isNegative = false;
-            ResetTextBox();
-            operation = "/";
         }
 
         private void btnEquals_Click(object sender, RoutedEventArgs e)
         {
-            isDecimal = false;
-            isNegative = false;
-
-            if (operation == "*")
+            operationPressed = false;
+            equation.Content = "";
+            switch (operation)
             {
-                txtBox.Text = (totalNumbers * total).ToString();
-                totalNumbers *= total;
-                total = 0;
+                case "+":
+                    result.Text =(value +Double.Parse(result.Text)).ToString(); 
+                    break;
+                case "-":
+                    result.Text = (value - Double.Parse(result.Text)).ToString();
+                    break;
+                case "/":
+                    result.Text = (value / Double.Parse(result.Text)).ToString();
+                    break;
+                case "*":
+                    result.Text = (value * Double.Parse(result.Text)).ToString();
+                    break;
+
+                default:
+                    break;
+                 
             }
-            else if (operation == "+")
-            {
-                txtBox.Text = (totalNumbers + total).ToString();
-                totalNumbers += total;
-                total = 0;
-            }
-            else if (operation == "/")
-            {
-                txtBox.Text = (totalNumbers / total).ToString();
-                totalNumbers /= total;
-                total = 0;
-            }
-            else if (operation == "-")
-            {
-                txtBox.Text = (totalNumbers - total).ToString();
-                totalNumbers -= total;
-                total = 0;
-            }
-        }
 
-        private void btnDecimal_Click(object sender, RoutedEventArgs e)
-        {
-            txtBox.Text = total + ".";
-            isDecimal = true;
-        }
-
-        private void IsDecimal()
-        {
-            decimalNumber = number1 * x;
-
-            total += decimalNumber;
-            x *= 0.1F;
-            decimalNumber = 0.1F;
-            txtBox.Text = total.ToString();
-        }
-
-        private void IsNegative()
-        {
-            if (isNegative == true)
-            {
-                number1 = number1 * -1;
-            }
-        }
-
-        public void ShowNumber()
-        {
-            total = (total * 10) + number1;
-            txtBox.Text = total.ToString();
-        }
-
-        public void ResetTextBox()
-        {
-            txtBox.Text = "0";
-            totalNumbers += total;
-            total = 0;
-        }
-
-        private void btnClear_Click(object sender, RoutedEventArgs e)
-        {
-            //clears Everything
-            total = 0;
-            totalNumbers = 0;
-            isDecimal = false;
-            isNegative = false;
-            operation = "";
-            txtBox.Text = "0";
         }
 
         private void btnClearEntry_Click(object sender, RoutedEventArgs e)
         {
-            //Clears everything on this action say 587 * 545 then it clears 545 only and 587 is still left
+            result.Text = "0";
+            equation.Content = "";
+        }
 
-            total = 0;
-            isDecimal = false;
-            isNegative = false;
-            txtBox.Text = "0";
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            result.Text = "0" ;
+            value = 0;
+            equation.Content = "";
         }
     }
 }
